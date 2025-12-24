4bc06fc3f519629add6967efb0a3f832ff216781
25LP5105 25LP5105 <25LP5105@G302-CC0181.local>
2025-12-24 11:28:08 +0800

    push

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 console.log("🔑 HF_TOKEN:", process.env.HF_TOKEN ? "✓ Олдсон" : "✗ Олдсонгүй");
 
 const express = require("express");
@@ -6,24 +6,25 @@ const cors = require("cors");
 const path = require("path");
 const imageRouter = require("./routes/ImageRouter");
 const textRouter = require("./routes/TextRouter");
-const textToTextRoutes = require('./routes/TextToText');
-
+const textToTextRoutes = require("./routes/TextToText");
 
 const app = express();
 const PORT = process.env.PORT || 1000;
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["https://ai-image-model-back-end.onrender.com"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api", imageRouter);
-app.use("/api/text-to-image", textRouter); 
-app.use('/api/text-to-text', textToTextRoutes);
+app.use("/api/text-to-image", textRouter);
+app.use("/api/text-to-text", textToTextRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
